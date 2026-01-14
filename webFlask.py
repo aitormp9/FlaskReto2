89df58ececcee7d4467b8142280aafdf081d586a
ikamindeguia24 <ikamindeguia24@lhusurbil.eus>
2026-01-14 11:53:21 +0100

    cambios en estadoBandera y estadoPartida

--- webFlask.py
+++ webFlask.py
@@ -1,8 +1,13 @@
 import requests
 from flask import Flask, render_template, request
+import threading
 
 app = Flask(__name__, template_folder='templates')
 urlApi = "http://localhost:8080/api/v1"
+#urlApi = "http://3.233.57.10:8080/api/v1/"
+
+estado_bandera = 'libre'
+jugador_bandera= None
 
 @app.route('/', methods=['GET', 'POST'])
 def inicio():
@@ -10,7 +15,6 @@ def inicio():
 
 @app.route('/estadoPartida', methods=['GET'])
 def estadoPartida():
-
     return render_template('estadoPartida.html', name="estadoPartida")
 
 @app.route('/jugadoresConectados', methods=['GET'])
@@ -25,9 +29,15 @@ def posiciones():
     ranking = response.json()
     return render_template('posiciones.html', name="posiciones", ranking=ranking)
 
-@app.route('/estadoBandera', methods=['GET'])
+@app.route('/estadoBandera', methods=['GET', 'POST'])
 def estadoBandera():
-    return render_template('estadoBandera.html', name="estadoBandera")
+        return render_template('estadoBandera.html', name="estadoBandera")
+
+def actualizar_bandera(nuevo_estado, jugador = None):
+    global estado_bandera
+    if nuevo_estado in ["libre", "capturada"]:
+        estado_bandera = nuevo_estado
+        jugador_bandera = jugador
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
