93d520da99867737aab167cd6b11346bf81b271a
Mendez <ikyemendez24@lhusurbil.eus>
2026-01-15 12:44:38 +0100

    Se ha creado un paquete de requests para la comunicación del juego con el backend de spring boot

---
+++ Juego/gamerequests/jugador.py
@@ -0,0 +1,43 @@
+import requests
+
+class GameClient:
+    #url de la conexión con la API
+    def __init__(self):
+        self.api_url = "http://3.233.57.10:8080/api/v1"
+
+    #Guardar el resumen de la partida
+    def save_game(self, players:dict[int,int],duration:int):
+        for id,score in players.items():
+            data = {
+                "jugadorId":id,
+                "duracion":duration,
+                "score":score
+            }
+            requests.post(f"{self.api_url}/partidas",json = data)
+ 
+    #Inicia sesión con el email
+    def login(self, email:str):
+        data = {
+            "email":email
+        }
+        response = requests.post(f"{self.api_url}/jugadores/login", json = data)
+        return response.json()
+        
+# ***** EJEMPLO DE IMPLEMENTACIÓN *****
+#def main():
+    
+    #partida = GameClient()
+    # ***** Guardar resumen de partida *****
+    #jugadores = {1:20000, 2:3000}
+    #duration = 5
+    #partida.save_game(jugadores,duration)
+    
+    # ***** Login de un jugador *****
+    #email = "ikyemendez24@lhusurbil.eus"
+    #j = partida.login(email)
+    #if "error" in j:
+     #   print(j["message"])
+    #else:
+     #   print(j["id"])
+    
+#main()
